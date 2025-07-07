d1a978062892ff398ec8bba7a9fd55f02ef7b986
KunalPradhan1 <kunal3421@gmail.com>
2025-07-06 19:34:34 -0700

    This is about halfway done with the backend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { WixClientContextProvider } from "@/context/wixContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <WixClientContextProvider>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </WixClientContextProvider>
+        </body>
     </html>
   );
 }
